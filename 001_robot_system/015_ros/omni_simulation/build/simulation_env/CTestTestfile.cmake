95717141ff48a8a5056aedc31cf15c6161bf7c7a
MAMMARU-class <hibiki3.maruyama@gmail.com>
2025-09-20 13:38:08 +0900

    delete needless folders

--- 001_robot_system/015_ros/omni_simulation/build/simulation_env/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hibiki/Robomech/Robo-MECS_exercise/001_robot_system/015_ros/omni_simulation/src/simulation_env
-# Build directory: /home/hibiki/Robomech/Robo-MECS_exercise/001_robot_system/015_ros/omni_simulation/build/simulation_env
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
